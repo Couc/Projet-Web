b64ada19fc2ff38d8e3bf58c6e6c4ea29f1203f3
Clement Boulachin <couc.boulac@gmail.com>
2012-09-15 11:52:31 +0200

    commit de samedi matin !!!

--- index.php
+++ index.php
@@ -1,78 +1,75 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <title>PMA</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<head>
+		<meta charset="utf-8">
+		<title>PMA</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<!-- Le styles -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!-- Le fav and touch icons -->
+		<!-- Script js-->
+		<script type="text/javascript"></script>
+	</head>
+	<body>
+		<?php
+		function lit_rss($fichier, $objets) {
 
-    <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-       
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+			// on lit tout le fichier
+			if ($chaine = @implode("", @file($fichier))) {
 
-    <!-- Le fav and touch icons -->
-  
-  </head>
+				// on découpe la chaine obtenue en items
+				$tmp = preg_split("/<\/?" . "item" . ">/", $chaine);
 
-  <body>
-    <?php
-    function lit_rss($fichier,$objets) {
+				// pour chaque item
+				for ($i = 1; $i < sizeof($tmp) - 1; $i += 2)
 
-  // on lit tout le fichier
-  if($chaine = @implode("",@file($fichier))) {
+				// on lit chaque objet de l'item
+					foreach ($objets as $objet) {
 
-    // on découpe la chaine obtenue en items
-    $tmp = preg_split("/<\/?"."item".">/",$chaine);
+						// on découpe la chaine pour obtenir le contenu de l'objet
+						$tmp2 = preg_split("/<\/?" . $objet . ">/", $tmp[$i]);
 
-    // pour chaque item
-    for($i=1;$i<sizeof($tmp)-1;$i+=2)
+						// on ajoute le contenu de l'objet au tableau resultat
+						$resultat[$i - 1][] = @$tmp2[1];
+					}
 
-      // on lit chaque objet de l'item
-      foreach($objets as $objet) {
+				// on retourne le tableau resultat
+				return $resultat;
+			}
+		}
 
-        // on découpe la chaine pour obtenir le contenu de l'objet
-        $tmp2 = preg_split("/<\/?".$objet.">/",$tmp[$i]);
+		function lit_rsss($fichier, $objets) {
 
-        // on ajoute le contenu de l'objet au tableau resultat
-        $resultat[$i-1][] = @$tmp2[1];
-      }
+			// on lit tout le fichier
+			if ($chaine = @implode("", @file($fichier))) {
 
-    // on retourne le tableau resultat
-    return $resultat;
-  }
-}
+				// on découpe la chaine obtenue en items
+				$tmp = preg_split("/<\/?" . "channel" . ">/", $chaine);
 
-function lit_rsss($fichier,$objets) {
+				// pour chaque item
+				for ($i = 1; $i < sizeof($tmp) - 1; $i += 2)
 
-  // on lit tout le fichier
-  if($chaine = @implode("",@file($fichier))) {
+				// on lit chaque objet de l'item
+					foreach ($objets as $objet) {
 
-    // on découpe la chaine obtenue en items
-    $tmp = preg_split("/<\/?"."channel".">/",$chaine);
+						// on découpe la chaine pour obtenir le contenu de l'objet
+						$tmp2 = preg_split("/<\/?" . $objet . ">/", $tmp[$i]);
 
-    // pour chaque item
-    for($i=1;$i<sizeof($tmp)-1;$i+=2)
+						// on ajoute le contenu de l'objet au tableau resultat
+						$resultat[$i - 1][] = @$tmp2[1];
+					}
 
-      // on lit chaque objet de l'item
-      foreach($objets as $objet) {
-
-        // on découpe la chaine pour obtenir le contenu de l'objet
-        $tmp2 = preg_split("/<\/?".$objet.">/",$tmp[$i]);
-
-        // on ajoute le contenu de l'objet au tableau resultat
-        $resultat[$i-1][] = @$tmp2[1];
-      }
-
-    // on retourne le tableau resultat
-    return $resultat;
-  }
-}
-?>
+				// on retourne le tableau resultat
+				return $resultat;
+			}
+		}
+	?>
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container-fluid">
@@ -113,43 +110,49 @@ function lit_rsss($fichier,$objets) {
     </a>
   </p>
 </div>
-<?php 
+<div class="row-fluid">
+	<div class="span2"></div>
+	<div class="span8">
+<div class="accordion" id="accordion2">
+<?php
 
-$rss = lit_rss("feed.xml",array("title","link","description","pubDate"));
+	$rss = lit_rss("feed.xml", array("title", "link", "description", "pubDate", "content:encoded","dc:creator"));
+	$i = 0;
+	foreach ($rss as $tab) {
+		$i++;
+		echo '	<div class="accordion-group">
+		 <div class="accordion-heading">
+		 <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse'.$i.'">
+		 '.$tab[0].'
+		 </a><img align="right" width="70px" src="./img/'.$tab[5].'.jpg"/>
+		 </div>
+		 <div id="collapse'.$i.'" class="accordion-body collapse">
+		 <div class="accordion-inner">
+		 '.$tab[4].'
+		 </div>
+		 </div>
+		 </div>';
 
-foreach($rss as $tab) {
-  echo '<div class="news_box">
-           <div class="news_box_title">'.$tab[0].'</div>
-           <div class="news_box_date">posté le '.date("d/m/Y",strtotime($tab[3])).'</div>
-           '.$tab[2].'</div>';
-}
+		/*echo '<div class="news_box" id="news_box'.$i.'" onclick="document.getElementById(\'new_'.$i.'\').style.display=\'block\'" onmouseover="this.style.backgroundColor = \'#B2B2B2\'" onmouseout="this.style.backgroundColor = \'\'">
+		 <div class="news_box_title">'.$tab[0].'</div><div id="new_'.$i.'" style="display:none;">'.$tab[4].'</div>
+		 <div class="news_box_date">posté le '.date("d/m/Y",strtotime($tab[3])).'</div>
+		 '.$tab[2].'</div>';
+		 */
+	}
+		?>
+		</div>
+</div>
+<div class="span2"></div>
 
-$rss1 = lit_rsss("feed.xml",array("link","image"));
-foreach($rss1 as $tab1) {
-  
- echo '<a href="'.$tab[0].'"><img src="'.$tab[1].'" alt="photo" /></a>';
-}
-  ?>      
-      </div><!-- row  -->
-    </div><!--/.fluid-container-->
+</div>
+		</div><!-- row  -->
+		</div><!--/.fluid-container-->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script type="text/javascript" src ="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"> </script>
-    <script src="js/bootstrap-transition.js"></script>
-    <!--<script src="js/bootstrap-alert.js"></script>-->
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
-   
-
-  </body>
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script type="text/javascript" src ="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+		<script src="./js/bootstrap.js"></script>
+		<!--<script src="js/bootstrap-alert.js"></script>-->
+	</body>
 </html>
