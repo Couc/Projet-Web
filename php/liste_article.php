a0aa173b23de8f8bb37d320f9bdab2b16cc8f18c
Clement Boulachin <couc.boulac@gmail.com>
2012-09-17 12:55:20 +0200

    mieux comme sa

--- php/liste_article.php
+++ php/liste_article.php
@@ -5,7 +5,11 @@
 //recuperation des paramêtres :
 		//Categorie
 		//login (si existant)
-		//
+
+		
+//Lecture de la table categorie
+	//Si la derniere maj est plus vieille que 10min,
+		//apelle maj_rss($categorie);
 //lecture de la table article avec cette catégorie (jointure categorie -> source)
 		//Si login existant, prendre les liens choisit par l'utilisateur (tous par défault)
 
@@ -15,7 +19,7 @@
 
 
 
-//Fonction maj_rss()
+//Fonction maj_rss($categorie)
 	//Select de chaque source de la categorie à mettre à jour
 	//pour chaque source :
 		//Recuperer le fichier flux.xml
