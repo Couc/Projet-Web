3babe49ca99034f275b21edf615e9d2ac2f3186d
Clement Boulachin <couc.boulac@gmail.com>
2012-09-17 12:52:56 +0200

    description fonction maj_rss()

--- php/liste_article.php
+++ php/liste_article.php
@@ -14,4 +14,12 @@
 	//fin de script;
 
 
+
+//Fonction maj_rss()
+	//Select de chaque source de la categorie à mettre à jour
+	//pour chaque source :
+		//Recuperer le fichier flux.xml
+		//le parser
+		//ajouter chaque news dans la table
+//fin de fonction
 ?>
