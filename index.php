3ec131b00babfe418acb268c748698089de4549d
unknown <thomas@.(none)>
2012-11-21 09:47:22 +0100

    modif liste_article.php

--- index.php
+++ index.php
@@ -93,19 +93,37 @@ mysql_query("SET NAMES UTF8");
         	<div class="slider_content" style="width:100%;">
         		 <ul class="rslides" id="slider2" style="width:100%;">
         		 	<?php
-        		 	$sql_article="SELECT * FROM ARTICLE ORDER BY id_art DESC LIMIT 5;";
+        		 	$sql_article="SELECT * FROM ARTICLE a, SOURCE s WHERE a.id_source = s.id_source ORDER BY a.id_art DESC LIMIT 5;";
 					$query_article = mysql_query($sql_article) or die("ERREUR MYSQL numéro: " . mysql_errno() . "<br>Type de cette erreur: " . mysql_error() . "<br>\n");
-        		 	
+					
 					while($result_article=mysql_fetch_assoc($query_article)){
 						echo"<li class=\"article-accueil-slide\" style=\"float:left;position:relative;list-style-type:none;\">
         				        		
 			        		<div id=\"info-post-accueil\">
 			        			<i class=\"icon-calendar\" id=\"icone-accueil\"></i><date>Mon 06 Nov 2012</date>
 			        			<br>
-			        			<span id=\"author\" ><i class=\"icon-user\" id=\"icone-accueil\"></i>Korben</span>
-			        			<br>
-			        			<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>High-Tech</span>
-			        			<br>
+			        			<span id=\"author\" ><i class=\"icon-user\" id=\"icone-accueil\"></i>".$result_article['libelle']."</span>
+			        			<br>";
+								
+								switch($result_article['id_cat']){
+									case 1:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Politique</span>";
+										break;
+									case 2:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>High-Tech</span>";
+										break;
+									case 3:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Sport</span>";
+										break;
+									case 4:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Economie</span>";
+										break;
+									case 5:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>People</span>";
+										break;
+								}
+			        			
+			        			echo"<br>
 			        			<span id=\"nombre_commentaire\" ><i class=\"icon-comment\" id=\"icone-accueil\"></i>".$result_article['nb_comment']." comments</span>
 			        			<br>
         						<span id=\"nombre_likes\" ><i class=\"icon-thumbs-up\" id=\"icone-accueil-last\" ></i>".$result_article['nb_like']." likes</span>
@@ -122,11 +140,26 @@ mysql_query("SET NAMES UTF8");
 			        				<div id=\"info-post-tablet\">	
 		        						<i class=\"icon-calendar\" id=\"icone-accueil-tablet-first\"></i><date style=\"float:left;\">Mon 06 Nov 2012</date>
 					        			
-					        			<span id=\"author\" id=\"icone-accueil-tablet\"><i class=\"icon-user\" style=\"margin-right:4px;\"></i>Korben</span>
+					        			<span id=\"author\" id=\"icone-accueil-tablet\"><i class=\"icon-user\" style=\"margin-right:4px;\"></i>".$result_article['libelle']."</span>";
 					        			
-					        			<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>High-Tech</span>
-					        			
-					        			<span id=\"nombre_commentaire\" id=\"icone-accueil-tablet\"><i class=\"icon-comment\" style=\"margin-right:4px;\"></i>".$result_article['nb_comment']." comments</span>
+					        			switch($result_article['id_cat']){
+											case 1:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Politique</span>";
+												break;
+											case 2:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>High-Tech</span>";
+												break;
+											case 3:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Sport</span>";
+												break;
+											case 4:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Economie</span>";
+												break;
+											case 5:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>People</span>";
+												break;
+										}
+					        			echo "<span id=\"nombre_commentaire\" id=\"icone-accueil-tablet\"><i class=\"icon-comment\" style=\"margin-right:4px;\"></i>".$result_article['nb_comment']." comments</span>
 					        			
 					        			<span id=\"nombre_likes\" id=\"icone-accueil-tablet-last\"><i class=\"icon-thumbs-up\" style=\"margin-right:4px;\"></i>".$result_article['nb_like']." likes</span>
         							</div>
@@ -152,7 +185,7 @@ mysql_query("SET NAMES UTF8");
         		</div>
         		
         		<?php
-        		$sql_article="SELECT * FROM ARTICLE ORDER BY id_art DESC LIMIT 30;";
+        		$sql_article="SELECT * FROM ARTICLE a, SOURCE s WHERE a.id_source = s.id_source ORDER BY a.id_art DESC LIMIT 30;";
 				$query_article=mysql_query($sql_article) or die("ERREUR MYSQL numéro: " . mysql_errno() . "<br>Type de cette erreur: " . mysql_error() . "<br>\n");
         		 	
 					while($result_article=mysql_fetch_assoc($query_article)){
@@ -163,11 +196,27 @@ mysql_query("SET NAMES UTF8");
         		<div id=\"info-post-accueil\" >
         			<i class=\"icon-calendar\" id=\"icone-accueil\" ></i><date>Mon 06 Nov 2012</date>
         			<br>
-        			<span id=\"author\" ><i class=\"icon-user\" id=\"icone-accueil\" ></i>Korben</span>
-        			<br>
-        			<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\" ></i>High-Tech</span>
-        			<br>
-        			<span id=\"nombre_commentaire\" ><i class=\"icon-comment\" id=\"icone-accueil\" ></i>".$result_article['nb_comment']." comments</span>
+        			<span id=\"author\" ><i class=\"icon-user\" id=\"icone-accueil\" ></i>".$result_article['libelle']."</span>
+        			<br>";
+					
+        			switch($result_article['id_cat']){
+									case 1:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Politique</span>";
+										break;
+									case 2:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>High-Tech</span>";
+										break;
+									case 3:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Sport</span>";
+										break;
+									case 4:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>Economie</span>";
+										break;
+									case 5:
+										echo"<span id=\"categorie\" ><i class=\"icon-tasks\" id=\"icone-accueil\"></i>People</span>";
+										break;
+								}
+        			echo"<br><span id=\"nombre_commentaire\" ><i class=\"icon-comment\" id=\"icone-accueil\" ></i>".$result_article['nb_comment']." comments</span>
         			<br>
         			<span id=\"nombre_likes\" ><i class=\"icon-thumbs-up\" id=\"icone-accueil-last\" ></i>".$result_article['nb_like']." likes</span>
         		</div>
@@ -181,11 +230,26 @@ mysql_query("SET NAMES UTF8");
         				<div id=\"info-post-tablet\">	
         						<i class=\"icon-calendar\" id=\"icone-accueil-tablet-first\"></i><date style=\"float:left;\">Mon 06 Nov 2012</date>
 			        			
-			        			<span id=\"author\" id=\"icone-accueil-tablet\" ><i class=\"icon-user\" style=\"margin-right:4px;\"></i>Korben</span>
-			        			
-			        			<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>High-Tech</span>
-			        			
-			        			<span id=\"nombre_commentaire\" id=\"icone-accueil-tablet\"><i class=\"icon-comment\" style=\"margin-right:4px;\"></i>".$result_article['nb_comment']." comments</span>
+			        			<span id=\"author\" id=\"icone-accueil-tablet\" ><i class=\"icon-user\" style=\"margin-right:4px;\"></i>".$result_article['libelle']."</span>";
+					        			
+					        			switch($result_article['id_cat']){
+											case 1:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Politique</span>";
+												break;
+											case 2:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>High-Tech</span>";
+												break;
+											case 3:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Sport</span>";
+												break;
+											case 4:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>Economie</span>";
+												break;
+											case 5:
+												echo "<span id=\"categorie\" id=\"icone-accueil-tablet\"><i class=\"icon-tasks\" style=\"margin-right:4px;\"></i>People</span>";
+												break;
+										}
+					        			echo "<span id=\"nombre_commentaire\" id=\"icone-accueil-tablet\"><i class=\"icon-comment\" style=\"margin-right:4px;\"></i>".$result_article['nb_comment']." comments</span>
 			        			
 			        			<span id=\"nombre_likes\" id=\"icone-accueil-tablet-last\"><i class=\"icon-thumbs-up\" style=\"margin-right:4px;\"></i>".$result_article['nb_like']." likes</span>
         				</div>
